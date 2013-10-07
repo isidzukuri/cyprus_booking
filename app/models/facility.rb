773e943c4d35bed3fc4c8bbef913de62a64ff63a
adok <adok@ukr.net>
2013-10-07 17:10:51 +0300

    fixed

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -3,7 +3,6 @@ class Facility < ActiveRecord::Base
 
   attr_accessible :id, :active, :name_ru, :name_uk, :name_en, :seo
 
-  # has_many :penalties
   has_and_belongs_to_many :houses
 
   validates :name_uk,:name_ru, :name_en,   :presence => {:message=>I18n.t("facilities.errors.presense")}, :length => {:minimum => 3, :maximum => 254 ,:message=>I18n.t("facilities.errors.minimum_chars")}
