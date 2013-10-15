3c28dc778f75dfe0c2c1b27108207a4ae0aa1817
adok <adok@ukr.net>
2013-10-15 14:41:10 +0300

    ifxed

--- app/models/apartments/facility.rb
+++ app/models/apartments/facility.rb
@@ -2,7 +2,7 @@ class Facility < ActiveRecord::Base
 
   attr_accessible :id, :active, :name_ru, :name_uk, :name_en, :seo ,:ico_file_name
   has_attached_file :ico,
-  	:url  => "/ініеуь/facilities/:id/:style.:extension",
+  	:url  => "/facilities/:id/:style.:extension",
     :path => ":rails_root/public/system/facilities/:id/:style.:extension", 
     :styles => {
       :small  => "35x35>"
